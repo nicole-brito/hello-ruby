c2659ced3e919250aab33c4df398fdcba0090606
Nicole Brito <80689302+nicole-brito@users.noreply.github.com>
2022-04-22 20:31:46 -0300

    Create hello_world.rb

---
+++ hello_world.rb
@@ -0,0 +1,14 @@
+#Isso é um comentário
+puts "Hello World!"
+nome = "Nicole"
+
+#interpolação
+mensagem = "Bem vindo #{nome}"
+
+puts mensagem
+
+#outra forma de interpolação 
+conta = "2 + 2 = #{1 + 3}"
+puts conta
+
+puts "O tamanho da minha string é #{mensagem.length}" 
