54a1551d8a448d5ade7c11af1139ca128b5d8bd6
Nicole Brito <nicolem.britto@gmail.com>
2022-06-30 21:07:11 -0300

    Create 02_22_classes.rb

---
+++ modulo_02/02_22_classes.rb
@@ -0,0 +1,21 @@
+#Classes: forma / objeto: objeto pronto.
+
+#Métodos: são as acções / Propriedades: são as características
+
+#Uma classe instanciada é um objeto
+
+#Uma forma de instanciar uma classe é por inferencia ou por declaração explicíta
+
+# Por inferencia: a = [1,2,3]
+# Por declaração explicíta: a = Array.new([1,2,3])
+
+#Isso é uma classe: (a primeira letra maiúscula)
+class PessoaFisica
+    def falar
+        "Olá"
+    end
+end
+
+#Métodos: ações
+#Propriedades: características
+
