12cefaca8ef53714c2b03ae5ff98c5fd6ef8acac
Nicole Brito <nicolem.britto@gmail.com>
2022-07-11 19:16:12 -0300

    Aula 30

---
+++ modulo_02/02_30_bundler.rb
@@ -0,0 +1,5 @@
+#Dependencias: Algumas gems dependem de outras gems.
+
+#O bundler ajuda a instalar as gems necessárias para o projeto.
+
+#Crie um arquivo Gemfile, adicione um repositorio do github (source 'https://rubygems.org') e instale as gems que precisa
