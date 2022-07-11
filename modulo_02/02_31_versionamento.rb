c54c339b3b50984ff33eb6bfe6df29f92c45aead
Nicole Brito <nicolem.britto@gmail.com>
2022-07-11 19:46:28 -0300

    Aula 31

---
+++ modulo_02/02_31_versionamento.rb
@@ -0,0 +1,12 @@
+=begin
+Sistema Comum de Versionamento
+X.Y.Z = Major.Minor.Patch
+
+1. PATCH é um bug fix, que corrige um erro ou um problema.
+2. MINOR é para alterações menores, que não afetam o funcionamento do sistema.
+3. MAJOR é para alterações maiores, que afetam o funcionamento do sistema.
+
+    gem 'pipipi', '1.1.1' = Versão exata
+    gem 'popopo', '>=1.1.1', = A partir da versão x.x.x
+    gem 'lerolero' '~>1.5.0' = Versão 'parcial' atual (Aproximadamente esse versão)
+=end
